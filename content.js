fe37dbe70b1ef4437d840dcf494b6ff4fc488cbf
Yakir Kahana <Yakir.kah@gmail.com>
2018-11-18 08:44:20 +0200

    popup element created properly

--- content.js
+++ content.js
@@ -1,19 +1,25 @@
+(function(){
 
-document.body.innerHTML += `<div id="to-shekel-popup"></div>`
-let popup = document.getElementById('to-shekel-popup');
-browser.runtime.onMessage.addListener((data)=>{
+    //document.body.innerHTML += `<div id="to-shekel-popup"></div>`
+    let popup = document.createElement("DIV");
+    popup.setAttribute("id","to-shekel-popup");
+    popup.appendChild(document.createTextNode(""));
+    document.body.appendChild(popup);
+    //let popup = document.getElementById('to-shekel-popup');
+    browser.runtime.onMessage.addListener((data) => {
 
-    popup.innerText = data;
-    popup.style.display = "inline-block";
-    setTimeout(() => { popup.className = "show";},10);
-    
+        popup.innerText = data;
+        popup.style.display = "inline-block";
+        setTimeout(() => { popup.className = "show"; }, 10);
 
-    setTimeout(()=>{
-        popup.className = "";
-        setTimeout(()=>{
-            popup.style.display = "none";
-            browser.runtime.sendMessage({done:true}).then();
-        },510);
-        
-    },3000);
-});
+
+        setTimeout(() => {
+            popup.className = "";
+            setTimeout(() => {
+                popup.style.display = "none";
+                browser.runtime.sendMessage({ done: true }).then();
+            }, 510);
+
+        }, 3000);
+    });
+}());
