2a760f8ce17b880ace65dfdce615bbf6d80397ea
Yakir Kahana <YakirKahana@protonmail.com>
2022-04-15 16:12:37 +0300

    get rates from the european central bank

--- bg.js
+++ bg.js
@@ -1,10 +1,70 @@
 let displayingPopup = false;
 
-fetch('https://api.ratesapi.io/api/latest?symbols=USD,EUR&base=ILS').then((response) => response.json()).then((json) => {
-  browser.storage.local.clear().then(() => {
-    browser.storage.local.set(json);
+// fetch('https://api.ratesapi.io/api/latest?symbols=USD,EUR&base=ILS').then((response) => response.json()).then((json) => {
+//   browser.storage.local.clear().then(() => {
+//     browser.storage.local.set(json);
+//   });
+// });
+
+let rates = {
+  USD: 0.0,
+  ILS: 0.0,
+  GBP: 0.0,
+};
+
+function mapAttrsToObj(data) {
+  console.log('data', data);
+  const array = Array.prototype.slice.call(data);
+  array.forEach((cube) => {
+    currency = cube.getAttribute('currency');
+    switch (currency) {
+      case 'USD':
+        rates.USD = parseFloat(cube.getAttribute('rate'));
+        break;
+
+      case 'ILS':
+        rates.ILS = parseFloat(cube.getAttribute('rate'));
+        break;
+
+      case 'GBP':
+        rates.GBP = parseFloat(cube.getAttribute('rate'));
+        break;
+    }
   });
-});
+  return rates;
+}
+
+function convertBaseToShekel(EuroBasedRates) {
+  return new Promise((resolve) => {
+    const newRates = {
+      EUR: 1 / EuroBasedRates.ILS,
+      USD: EuroBasedRates.USD / EuroBasedRates.ILS,
+      GBP: EuroBasedRates.GBP / EuroBasedRates.ILS,
+      date: EuroBasedRates.date,
+    };
+
+    resolve(newRates);
+  });
+}
+
+function getRates() {
+  return fetch('https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml')
+    .then((response) => response.text())
+    .then((data) => {
+      const pasrer = new DOMParser();
+      const xml = pasrer.parseFromString(data, 'text/xml');
+      const date = xml.getElementsByTagName('Cube')[1].getAttribute('time');
+      const RecivedRates = mapAttrsToObj(xml.getElementsByTagName('Cube'));
+      RecivedRates.date = date;
+      return RecivedRates;
+    });
+}
+getRates()
+  .then((EuroBasedRates) => convertBaseToShekel(EuroBasedRates))
+  .then((convertedRates) => {
+    rates = convertedRates;
+    console.log(convertedRates);
+  });
 
 browser.contextMenus.create({
   id: 'usd_to_ils',
